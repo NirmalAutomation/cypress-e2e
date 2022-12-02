c9572a61dbdbc2c7c00cc3839df79ceece5ce782
NirmalAutomation <nirmalthanga@gmail.com>
2022-12-02 18:38:16 +0530

    github actions

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: 'z5u76a',
   e2e: {
-    baseUrl: 'https://www.hotstar.com/in'
+    baseUrl: 'https://www.medicaid.gov'
   },
   viewportHeight: 800,
   viewportWidth: 1200,
--- cypress/e2e/landingPage.cy.ts
+++ cypress/e2e/landingPage.cy.ts
@@ -1,17 +1,24 @@
+/// <reference types="Cypress" />
+
 describe('Navigate to Landing Page', () => {
 
     beforeEach(() => {
-
-        cy.visit('/', {failOnStatusCode: false});
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
+        cy.visit('/' ,{failOnStatusCode: false});
 
     })
 
     it('Assert title in Landing Page', () => {
 
-        cy.wait(10000)
-        cy.get('.nav-container').contains('Movies').click();
+        cy.get('#site-header').contains('Federal Policy Guidance').click()
+            .then(() => {
+                cy.get('h1').should('contain.text', 'Federal Policy Guidance');
+            })
 
     })
 })
 
 //git push --set-upstream origin init
+// npx cypress run --record --key 11233911-e9d2-4c22-b6ea-6fb6a49b5c25
