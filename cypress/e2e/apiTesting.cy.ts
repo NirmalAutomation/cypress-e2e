cbc0d657f9909ead79c849b63745fd45b11ad9b4
NirmalAutomation <nirmalthanga@gmail.com>
2022-12-04 15:31:01 +0530

    included api testing

---
+++ cypress/e2e/apiTesting.cy.ts
@@ -0,0 +1,44 @@
+import 'cypress-plugin-api'
+
+describe('Rest API Testing', () => {
+
+    it('Test POST Request', () => {
+        cy.api({
+            method: 'POST',
+            url: 'https://rahulshettyacademy.com/maps/api/place/add/json?qaclick123',
+            headers: {
+                accept: 'application/json'
+            },
+            body: {
+                "location": {
+                    "lat": -38.383494,
+                    "lng": 33.427362
+                },
+                "accuracy": 50,
+                "name": "Frontline house",
+                "phone_number": "(+91) 983 893 3937",
+                "address": "29, side layout, cohen 09",
+                "types": [
+                    "shoe park",
+                    "shop"
+                ],
+                "website": "http://google.com",
+                "language": "French-IN"
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+        })
+    })
+
+    it('Test GET Request', () => {
+        cy.api({
+            method: 'GET',
+            url: 'http://rahulshettyacademy.com/maps/api/place/get/json?place_id=0ce9501466dc63272227742a503f9895&key=qaclick123',
+
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.isOkStatusCode).to.eq(true)
+
+        });
+    });
+});
--- cypress/e2e/landingPage.cy.ts
+++ cypress/e2e/landingPage.cy.ts
@@ -1,28 +1,37 @@
 /// <reference types="Cypress" />
 
+import landingPage from "../pages/landingPage";
+
 describe('Navigate to Landing Page', () => {
 
     beforeEach(() => {
         Cypress.on('uncaught:exception', (err, runnable) => {
             return false
         })
-        cy.visit('/' ,{failOnStatusCode: false});
+        cy.visit('/', {failOnStatusCode: false});
 
     })
-
-    it('Assert title', () => {
-        cy.get('[data-test="@web/Header/MainMenuLink"]')
-            .contains('Categories').click();
-    })
-
-    // it('Assert title in Landing Page', () => {
     //
-    //     cy.get('#site-header').contains('Federal Policy Guidance').click()
-    //         .then(() => {
-    //             cy.get('h1').should('contain.text', 'Federal Policy Guidance');
-    //         })
+    // it('Verify the Main menu links', () => {
+    //     const categories = new landingPage();
+    //     categories
+    //         .mainMenuLinks().eq(0)
+    //         .should('contain.text', 'Categories');
     //
+    //     categories
+    //         .mainMenuLinks().eq(1)
+    //         .should('contain.text', 'Deals');
+    //
+    //     categories
+    //         .mainMenuLinks().eq(2)
+    //         .click();
     // })
+
+    it('Assert title in Landing Page', () => {
+
+        cy.get('.unav-header__nav').contains('Plans').click();
+
+    })
 })
 
 //git push --set-upstream origin init
