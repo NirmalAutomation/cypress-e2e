0bb31886ea2147761dc1c524ee5c334e783be879
NirmalAutomation <nirmalthanga@gmail.com>
2022-12-04 16:05:51 +0530

    intial ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   projectId: 'z5u76a',
   e2e: {
-    baseUrl: 'https://www.t-mobile.com/'
+    baseUrl: 'https://www.target.com'
   },
   viewportHeight: 800,
   viewportWidth: 1200,
