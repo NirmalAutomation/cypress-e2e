936e98fd3af1de499c845fcd58fc6cbc2ff5c5e4
NirmalAutomation <35673592+NirmalAutomation@users.noreply.github.com>
2022-12-16 22:40:06 +0530

    Update landingPage.cy.ts

--- cypress/e2e/landingPage.cy.ts
+++ cypress/e2e/landingPage.cy.ts
@@ -16,20 +16,3 @@ describe('Navigate to Landing Page', () => {
     })
 
 })
-
-//git push --set-upstream origin init
-// npx cypress run --record --key 11233911-e9d2-4c22-b6ea-6fb6a49b5c25
-
-// For Mac
-// docker run -it -v $PWD:/e2e -w /e2e cypress/included:11.2.0 --browser chrome
-
-// For Windows
-//docker run -it -v ${PWD}:/e2e -w /e2e cypress/included:9.2.0
-
-// Docker
-// docker build -t my-cypress-image:2.0 .
-// docker run -it -v $PWD:/my-cypress-project -t my-cypress-image:2.0 cypress/e2e/*.cy.ts --browser chrome
-
-// Docker Compose
-// docker-compose up (To run all mentioned browser)
-// docker-compose run e2e-chrome
